8fc7c245ed82f8a66b9b0283cde13b3e110a2ea3
missahrizona <princessssahri@gmail.com>
2022-12-30 03:18:38 -0600

    added websocket server listener

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
+
+const app = express();
+//app.use(express.static(path.join(__dirname, '/public')));
+
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  console.log('new conneciton established')
+  ws.on('message', function(msgStr) {
+    const msg = JSON.parse(msgStr)
+    console.log(msg)
+  })
+
+  ws.on('close', function () {
+    console.log('closing client connection');
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Listening on http://0.0.0.0:8080');
+});
