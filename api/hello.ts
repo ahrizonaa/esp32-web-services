2c5a59219e0a1fe2d6b9a0cc58f4b0fbd702dbdd
missahrizona <princessssahri@gmail.com>
2022-12-30 00:01:41 -0600

    allow cors

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,23 @@
 import fetch from 'node-fetch';
 
-export default async function handler(request, response) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+export default allowCors(async function handler(request, response) {
   // const res = await fetch('...', {
   //   method: 'POST',
   //   body: JSON.stringify({
@@ -12,4 +29,4 @@ export default async function handler(request, response) {
 
   // const data = await res.json();
   return response.status(200).json({ msg: 'hello' });
-}
+})
