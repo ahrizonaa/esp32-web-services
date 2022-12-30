92c5efddcf6f011dcef0bb81ce6e1d87ee3aa409
missahrizona <princessssahri@gmail.com>
2022-12-30 05:40:16 -0600

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const { createServer } = require('http');
 
 const WebSocket = require('ws');
 
 const app = express();
+app.use(cors())
 //app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
